d293ee41f039a76350224a695c16617b5a198639
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-11-16 14:58:56 +0530

    verifying otp in signup and creating jwt

--- express-server/controllers/user.controllers.js
+++ express-server/controllers/user.controllers.js
@@ -3,13 +3,14 @@ const HttpStatus = require("http-status-codes");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const otplib = require("otplib");
+const jwt = require("jsonwebtoken");
 
 const { mailgunHelper } = require("../config/mailgun");
+const { otplibAuthenticator } = require("../config/otplib");
 
 const login = async (req, res) => {
   console.log("login route");
-  res.json({ message: "Login route" });
+  res.json({ msg: "Login route" });
 };
 
 const signup = async (req, res) => {
@@ -29,26 +30,26 @@ const signup = async (req, res) => {
 
     const userExists = await User.findOne({ email });
 
-    if (userExists) {
-      if (!userExists.verified) {
-        const otp = otplib.authenticator.generate(userExists.email);
+    if (userExists && !userExists.verified) {
+      const otp = otplibAuthenticator.generate(userExists.email);
 
-        const mailData = {
-          from: process.env.MAILGUN_FROM,
-          to: userExists.email,
-          subject: `Your OTP is ${otp}`,
-          text: `Your OTP for MERN Authentication is ${otp}`
-        };
+      const mailData = {
+        from: process.env.MAILGUN_FROM,
+        to: userExists.email,
+        subject: `Your OTP is ${otp}`,
+        text: `Your OTP for MERN Authentication is ${otp}`
+      };
 
-        await mailgunHelper.messages().send(mailData);
+      await mailgunHelper.messages().send(mailData);
 
-        return res.json({
-          success: true,
-          message: "User already exists. OTP sent to your email.",
-          userExists
-        });
-      }
+      return res.json({
+        success: true,
+        msg: "User already exists. OTP sent to your email.",
+        userExists
+      });
+    }
 
+    if (userExists && userExists.verified) {
       return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
         success: false,
         errors: [{ msg: "User already exists. Please login." }]
@@ -59,7 +60,7 @@ const signup = async (req, res) => {
 
     const user = await User.create({ email, password });
 
-    const otp = otplib.authenticator.generate(user.email);
+    const otp = otplibAuthenticator.generate(user.email);
 
     const mailData = {
       from: process.env.MAILGUN_FROM,
@@ -72,7 +73,7 @@ const signup = async (req, res) => {
 
     return res.json({
       success: true,
-      message: "User created successfully. OTP sent to your email.",
+      msg: "User created successfully. OTP sent to your email.",
       user
     });
   } catch (err) {
@@ -84,4 +85,72 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = { login, signup };
+const verifyOtp = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
+        success: false,
+        errors: errors.array()
+      });
+    }
+
+    let { email, otp } = req.body;
+
+    email = email.toLowerCase();
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists && !userExists.verified) {
+      console.log(otp, userExists.email);
+      const isValid = otplibAuthenticator.verify({
+        token: otp,
+        secret: userExists.email
+      });
+
+      if (!isValid) {
+        return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
+          success: false,
+          errors: [{ msg: "Invalid OTP. Please check OTP and try again." }]
+        });
+      }
+
+      userExists.verified = true;
+
+      await userExists.save();
+
+      const jwtToken = jwt.sign(
+        { _id: String(userExists._id), email: userExists.email },
+        process.env.JWT_SECRET
+      );
+
+      return res.json({
+        success: true,
+        msg: "Registered successfully. Logged in successfully.",
+        user: userExists,
+        jwt: jwtToken
+      });
+    }
+
+    if (userExists && userExists.verified) {
+      return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
+        success: false,
+        errors: [{ msg: "User already exists. Please login." }]
+      });
+    }
+
+    return res.status(HttpStatus.NOT_FOUND).json({
+      success: false,
+      errors: [{ msg: "User not found. Please signup." }]
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      errors: [{ msg: "Internal server error" }]
+    });
+  }
+};
+
+module.exports = { login, signup, verifyOtp };
--- express-server/routes/user.routes.js
+++ express-server/routes/user.routes.js
@@ -2,7 +2,15 @@ const router = require("express").Router();
 const UserControllers = require("../controllers/user.controllers");
 const { check } = require("express-validator");
 
-router.post("/login", UserControllers.login);
+router.post(
+  "/login",
+  [
+    check("email") // required + email
+      .isEmail()
+      .withMessage("Please enter valid email address.")
+  ],
+  UserControllers.login
+);
 router.post(
   "/signup",
   [
@@ -14,6 +22,18 @@ router.post(
       .withMessage("Please enter a password with atleast 5 characters")
   ],
   UserControllers.signup
+);
+router.post(
+  "/verifyotp",
+  [
+    check("email") // required + email
+      .isEmail()
+      .withMessage("Please enter valid email address."),
+    check("otp") // required + length 6
+      .isLength({ min: 6, max: 6 })
+      .withMessage("Please enter a OTP that is 6 digits.")
+  ],
+  UserControllers.verifyOtp
 );
 
 module.exports = router;
