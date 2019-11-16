7f250f4f9abe942680679c75484de9d041bfacd7
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-11-16 13:49:42 +0530

    simple signup route with validation

--- express-server/controllers/user.controllers.js
+++ express-server/controllers/user.controllers.js
@@ -1,5 +1,8 @@
 const User = require("../models/user.model");
 const HttpStatus = require("http-status-codes");
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const login = async (req, res) => {
   console.log("login route");
@@ -7,8 +10,47 @@ const login = async (req, res) => {
 };
 
 const signup = async (req, res) => {
-  const { email, password } = req.body;
-  res.json({ email, password });
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
+        success: false,
+        errors: errors.array()
+      });
+    }
+
+    let { email, password } = req.body;
+
+    email = email.toLowerCase();
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res
+        .status(HttpStatus.UNPROCESSABLE_ENTITY)
+        .json({
+          success: false,
+          errors: [{ msg: "User Already Exists. Please Login." }]
+        });
+    }
+
+    password = await bcrypt.hash(password, saltRounds);
+
+    const user = await User.create({ email, password });
+
+    return res.json({
+      success: true,
+      message: "User Created Successfully.",
+      user
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      errors: [{ msg: "Internal Server Error" }]
+    });
+  }
 };
 
 module.exports = { login, signup };
--- express-server/routes/user.routes.js
+++ express-server/routes/user.routes.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
 const UserControllers = require("../controllers/user.controllers");
+const { check } = require("express-validator");
 
 router.post("/login", UserControllers.login);
-router.post("/signup", UserControllers.signup);
+router.post(
+  "/signup",
+  [
+    check("email") // required + email
+      .isEmail()
+      .withMessage("Please enter valid email address."),
+    check("password") // required + min length 5
+      .isLength({ min: 5 })
+      .withMessage("Please enter a password with atleast 5 characters")
+  ],
+  UserControllers.signup
+);
 
 module.exports = router;
