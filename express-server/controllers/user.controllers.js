f186ec771ee654b8d2ce9e2f2d511dd4598fe5df
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-11-16 14:22:44 +0530

    user creation and sending otp using mailgun

--- express-server/controllers/user.controllers.js
+++ express-server/controllers/user.controllers.js
@@ -3,6 +3,9 @@ const HttpStatus = require("http-status-codes");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const otplib = require("otplib");
+
+const { mailgunHelper } = require("../config/mailgun");
 
 const login = async (req, res) => {
   console.log("login route");
@@ -27,28 +30,56 @@ const signup = async (req, res) => {
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-      return res
-        .status(HttpStatus.UNPROCESSABLE_ENTITY)
-        .json({
-          success: false,
-          errors: [{ msg: "User Already Exists. Please Login." }]
+      if (!userExists.verified) {
+        const otp = otplib.authenticator.generate(userExists.email);
+
+        const mailData = {
+          from: process.env.MAILGUN_FROM,
+          to: userExists.email,
+          subject: `Your OTP is ${otp}`,
+          text: `Your OTP for MERN Authentication is ${otp}`
+        };
+
+        await mailgunHelper.messages().send(mailData);
+
+        return res.json({
+          success: true,
+          message: "User already exists. OTP sent to your email.",
+          userExists
         });
+      }
+
+      return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
+        success: false,
+        errors: [{ msg: "User already exists. Please login." }]
+      });
     }
 
     password = await bcrypt.hash(password, saltRounds);
 
     const user = await User.create({ email, password });
 
+    const otp = otplib.authenticator.generate(user.email);
+
+    const mailData = {
+      from: process.env.MAILGUN_FROM,
+      to: user.email,
+      subject: `Your OTP is ${otp}`,
+      text: `Your OTP for MERN Authentication is ${otp}`
+    };
+
+    await mailgunHelper.messages().send(mailData);
+
     return res.json({
       success: true,
-      message: "User Created Successfully.",
+      message: "User created successfully. OTP sent to your email.",
       user
     });
   } catch (err) {
     console.log(err);
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
       success: false,
-      errors: [{ msg: "Internal Server Error" }]
+      errors: [{ msg: "Internal server error" }]
     });
   }
 };
